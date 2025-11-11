dc91d9124fdf2472c0c8d4d6556fe328314ce631
Joris Lefait <contact@joris-lefait.com>
2025-11-11 12:21:12 +0100

    carousel component autoplay

--- app/components/containers/MedicalInformations.vue
+++ app/components/containers/MedicalInformations.vue
@@ -8,14 +8,28 @@
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
       commodo consequat.
     </p>
-    <Button class="button" variant="secondary" href="/"
-      >Plus de ressources</Button
-    >
+    <Carousel :item-count="cards.length">
+      <article
+        v-for="(item, i) in cards"
+        :key="i"
+        :style="`--i:${i}`"
+        :item="item"
+        class="mi-card"
+      >
+        <img :src="item.icon" :alt="item.title" />
+        <h3>{{ item.title }}</h3>
+        <NuxtLink :to="item.link">En savoir plus</NuxtLink>
+      </article>
+    </Carousel>
+    <Button class="button" variant="secondary" href="/">
+      Plus de ressources
+    </Button>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Carousel from "../Carousel.vue";
 
 // données JSON : chaque objet peut contenir icon, title, link
 const cards = ref([
@@ -39,9 +53,21 @@ const cards = ref([
   },
   {
     id: "c4",
+    icon: "/images/tooth-brush.svg",
+    title: "Brossage de dents",
+    link: "/consultation",
+  },
+  {
+    id: "c5",
     icon: "/images/implant.svg",
     title: "L'implantologie",
     link: "/soins-dentaires",
+  },
+  {
+    id: "c6",
+    icon: "/images/post-op.svg",
+    title: "Soins post opératoires",
+    link: "/ophtalmo",
   },
 ]);
 </script>
@@ -69,12 +95,27 @@ const cards = ref([
     padding: 1rem;
   }
   .mi-card {
-    padding: $spacing-md;
-    background: #fff;
-    border-radius: 6$border-radius;
+    background: $color-white;
+    border-radius: $border-radius;
     box-shadow: 0 1px 6px rgba(0, 0, 0, 0.06);
-    min-width: 220px;
+    width: 250px;
+    height: 294px;
     text-align: center;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    img {
+      margin-top: $spacing-md;
+      height: 110px;
+    }
+    h3 {
+      margin-top: 1.5rem;
+    }
+    a {
+      margin-bottom: $spacing-md;
+    }
   }
 
   @media (max-width: 767px) {
